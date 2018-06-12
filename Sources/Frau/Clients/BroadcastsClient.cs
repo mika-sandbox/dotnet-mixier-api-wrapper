e6004afec4398d9b46d164b85594befa956a2b63
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-06-12 22:22:04 +0900

    :green_heart: pragma

--- Sources/Frau/Clients/BroadcastsClient.cs
+++ Sources/Frau/Clients/BroadcastsClient.cs
@@ -20,7 +20,10 @@ namespace Frau.Clients
             return await MixerClient.GetAsync<Broadcast>($"/broadcasts/{uuid}", null, false).Stay();
         }
 
+#pragma warning disable CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます
+
         public async Task<object> IdManifestAsync(string type)
+#pragma warning restore CS1998 // 非同期メソッドは、'await' 演算子がないため、同期的に実行されます
         {
             // TODO alias to /channels/{id}/manifest.{format}
             throw new NotImplementedException();
