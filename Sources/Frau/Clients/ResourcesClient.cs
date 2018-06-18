f814f7cd5771ccc6787a4187abf301993e1a1668
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-06-18 19:39:37 +0900

    :construction: Add /resources/* endpoint.

---
+++ Sources/Frau/Clients/ResourcesClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+using Frau.Enum;
+using Frau.Models;
+
+namespace Frau.Clients
+{
+    public class ResourcesClient : ApiClient
+    {
+        protected ResourcesClient(MixerClient client) : base(client) { }
+
+        public async Task<Resource> RetrievesAsync(uint resource)
+        {
+            return await MixerClient.GetAsync<Resource>($"/resources/{resource}");
+        }
+
+        public async Task DeleteAsync(uint resource)
+        {
+            await MixerClient.DeleteAsync($"/resources/{resource}", MediaType.Json);
+        }
+    }
+}
