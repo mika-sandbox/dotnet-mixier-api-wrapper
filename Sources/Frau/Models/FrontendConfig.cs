63f5f34715621b7e669c019965b84c546cbe4d60
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-06-24 22:47:29 +0900

    feat(frontendConfig): Add model.

---
+++ Sources/Frau/Models/FrontendConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+namespace Frau.Models
+{
+    public class FrontendConfig
+    {
+        [JsonProperty("alwaysOnline")]
+        public List<uint> AlwaysOnline { get; set; }
+
+        [JsonProperty("features")]
+        public object Features { get; set; }
+    }
+}
